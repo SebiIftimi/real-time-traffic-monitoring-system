7a98ff6addd049f8062ce7429aaa24a26e7ddb78
SebiIftimi <104670681+SebiIftimi@users.noreply.github.com>
2024-05-12 00:23:55 +0300

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -3,42 +3,41 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// LCD-ul
+// The LCD
 LiquidCrystal_I2C lcd(0x27, 20, 4); 
 
-// Pini pentru leduri masina
+// Pins for car LEDs
 const int LED_MAXSINA[] = {2, 3, 4, 5, 6};
 
-// Pin pentru LED-UL care indica ca a fost atins
-// nr maxim de masini
+// Pin for the LED that indicates the maximum number of cars has been reached
 #define LED_NR_MAX  7; 
 
-// NR_MAX_MASINI
+// Maximum number of cars
 const int nr_max = 5;
 
-//Pin buton panica
+// Panic button pin
 #define PIN_BUTON_PANICA  8;
 
-//Pin pentru ledul butonului de panica
+// Pin for the panic button LED
 #define LED_BUTON_PANICA  9; 
 
-//Pin SENZOR GAZ
+// GAS SENSOR pin
 #define PIN_SENZOR_GAZ  10;
 
-//Pin SENZOR GAZ
+// SMOKE SENSOR pin
 #define PIN_SENZOR_FUM 11;
 
-// Starile led-urilor, intrarii, iesirii,
-//butonul de panica
+// States of the LEDs, entrance, exit,
+// and panic button
 bool StareLeduri[5] = {false};
-bool BlocareIesire = false;
-bool BlocareIntrare = false;
-bool Panica = false;
+bool BlocareIesire = false; // Exit block
+bool BlocareIntrare = false; // Entrance block
+bool Panica = false; // Panic mode
 
-//Timere pentru led-uri
+// Timers for LEDs
 unsigned long ledTimers[5];
 
-//Semafoare 
+// Semaphores 
 SemaphoreHandle_t sem_intrare, sem_iesire;
 SemaphoreHandle_t sem_monitorizare;
 SemaphoreHandle_t sem_turnOnLED;
@@ -46,45 +45,47 @@ SemaphoreHandle_t sem_turnOnLED;
 // MUTEX
 SemaphoreHandle_t mutex_panica;
 
-// Cat timp ramane un led aprins/
-// Cat timp sta masina in tunel
+// How long an LED stays on/
+// How long a car stays in the tunnel
 long interval = 3000;
 
-// Prototipuri functii
-void Monitorizare(void *pvParameters);
-void Intrare_blocare(void *pvParameters);
-void Iesire(void *pvParameters);
-void Buton_Panica(void *pvParameters);
+// Function prototypes
+void Monitorizare(void *pvParameters); // Monitoring
+void Intrare_blocare(void *pvParameters); // Entry block
+void Iesire(void *pvParameters); // Exit
+void Buton_Panica(void *pvParameters); // Panic Button
 bool turnOnLEDS();
-int NrMasini();
+int NrMasini(); // Number of Cars
+
 
 void setup() {
-  //Initializare comunicatia seriala
+  // Initialize serial communication
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
 
+  // Creating binary semaphores for different functionalities
+  sem_intrare = xSemaphoreCreateBinary(); // Entrance semaphore
+  sem_iesire = xSemaphoreCreateBinary(); // Exit semaphore
+  sem_monitorizare = xSemaphoreCreateBinary(); // Monitoring semaphore
+  sem_serial_monitor = xSemaphoreCreateBinary(); // Serial monitor semaphore
+  sem_turnOnLED = xSemaphoreCreateBinary(); // LED control semaphore
+  mutex_panica = xSemaphoreCreateMutex(); // Panic mutex
 
-  sem_intrare = xSemaphoreCreateBinary();
-  sem_iesire = xSemaphoreCreateBinary();
-  sem_monitorizare = xSemaphoreCreateBinary();
-  sem_serial_monitor = xSemaphoreCreateBinary();
-  sem_turnOnLED = xSemaphoreCreateBinary();
-  mutex_panica = xSemaphoreCreateMutex();
-
-
+  // Set pin modes for LEDs
   for (int i = 0; i < nr_max; i++) {
     pinMode(LED_MAXSINA[i], OUTPUT);
   }
-  pinMode(LED_NR_MAX, OUTPUT); 
+  pinMode(LED_NR_MAX, OUTPUT);
   pinMode(PIN_BUTON_PANICA, INPUT_PULLUP); 
   pinMode(LED_BUTON_PANICA, OUTPUT); 
 
-  xTaskCreate(Monitorizare, "Monitorizare", 128, NULL, 1, NULL);
-  xTaskCreate(Intrare, "Intrare", 128, NULL, 1, NULL);
-  xTaskCreate(Iesire, "Iesire", 128, NULL, 1, NULL);
-  xTaskCreate(Buton_Panica, "ButonPanica", 100, NULL, 2, NULL);
-  xTaskCreate(Task_SenzorGaz, "SenzorGaz", 128, NULL, 1, NULL);
+  // Create tasks for different functionalities
+  xTaskCreate(Monitorizare, "Monitorizare", 128, NULL, 1, NULL); // Monitoring task
+  xTaskCreate(Intrare, "Intrare", 128, NULL, 1, NULL); // Entrance task
+  xTaskCreate(Iesire, "Iesire", 128, NULL, 1, NULL); // Exit task
+  xTaskCreate(Buton_Panica, "ButonPanica", 100, NULL, 2, NULL); // Panic button task
+  xTaskCreate(Task_SenzorGaz, "SenzorGaz", 128, NULL, 1, NULL); // Gas sensor task
 }
 
 void loop() {}
@@ -92,110 +93,106 @@ void loop() {}
 void Intrare(void *pvParameters) {
   (void) pvParameters;
   while(1) {
-    if (Serial.available() > 0 ) {
+    if (Serial.available() > 0) {
       char command = Serial.read();
       if (command == '1') {
         if (xSemaphoreTake(sem_intrare, (TickType_t)10) == pdTRUE) {
           if (Nrmasini() >= nr_max && Panica) {
-            // Tunelu e plin
-            // Intrare blocata
-            Serial.println("Intrare blocata");
-            // Aprinde LED-ul pentru NR_MAX
+            // The tunnel is full
+            // Entry blocked
+            Serial.println("Entry blocked");
+            // Light up the LED for NR_MAX
             digitalWrite(LED_NR_MAX, HIGH);
           } else if (turnOnLEDS()) {
-            Serial.print("A intrat o masina in tunel. Total masini: ");
+            Serial.print("A car entered the tunnel. Total cars: ");
             Serial.println(Nrmasini());
           }
           xSemaphoreGive(sem_intrare);
 
-          // Semnalizează turnOnLEDS
+          // Signal turnOnLEDS
           xSemaphoreGive(sem_turnOnLED);
-          // Semnalizează task-ul de monitorizare
+          // Signal the monitoring task
           xSemaphoreGive(sem_monitorizare);
-}
         }
       }
       if (command == '2') {
         if (sem_intrareTake(sem_intrare, (TickType_t)10) == pdTRUE) {
-          BlocareIesire = !BlocareIesire; // Comuta la  starea de blocare
+          BlocareIesire = !BlocareIesire; // Toggle the block state
           digitalWrite(LED_NR_MAX, BlocareIesire ? HIGH : LOW);
-          Serial.print("Sistemul este ");
-          Serial.println(BlocareIesire ? "BlocareIesire" : "deBlocareIesire");
+          Serial.print("The system is ");
+          Serial.println(BlocareIesire ? "blocking exit" : "unblocking exit");
           sem_intrareGive(sem_intrare);
-    }
-    vTaskDelay(50 / portTICK_PERIOD_MS);
-  }
-  if (command == '3') {
+        }
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+      }
+      if (command == '3') {
         if (sem_intrareTake(sem_intrare, (TickType_t)10) == pdTRUE) {
-            // Modifica starea de blocare a intrării
-            BlocareIntrare = !BlocareIntrare;
-            // Dacă blocăm intrarea și ieșirea este deja blocata,
-            // deblochează ieșirea
-            if (BlocareIntrare && BlocareIesire) {
-                xSemaphoreTake(mutex_panica, portMAX_DELAY);
-                // Deblochează ieșirea
-                BlocareIesire = false;
-                // Stinge LED-ul nr 7
-                digitalWrite(LED_NR_MAX, LOW);
-                xSemaphoreGive(mutex_panica);
-            }
-          Serial.print("Intrarea este ");
-          Serial.println(BlocareIntrare ? "BlocareIesirea" : "deBlocareIesirea");
+          // Change the state of entrance blocking
+          BlocareIntrare = !BlocareIntrare;
+          // If blocking the entrance and the exit is already blocked,
+          // unlock the exit
+          if (BlocareIntrare && BlocareIesire) {
+            xSemaphoreTake(mutex_panica, portMAX_DELAY);
+            // Unblock the exit
+            BlocareIesire = false;
+            // Turn off the LED number 7
+            digitalWrite(LED_NR_MAX, LOW);
+            xSemaphoreGive(mutex_panica);
+          }
+          Serial.print("The entrance is ");
+          Serial.println(BlocareIntrare ? "blocking exit" : "unblocking exit");
           sem_intrareGive(sem_intrare);
         }
       }
+    }
   }
 }
 
-
-
-//Afiseaza nr de masini pe LCD
+// Display the number of cars on the LCD
 void Monitorizare(void *pvParameters) {
   (void) pvParameters;
-  //Setare cursor la inceputul primei linii
-  lcd.setCursor(0, 0); 
-  lcd.print("Masini: ");
+  // Set cursor at the beginning of the first line
+  lcd.setCursor(0, 0);
+  lcd.print("Cars: ");
 
   while(1) {
     if (sem_intrareTake(sem_monitorizare, (TickType_t)10) == pdTRUE) {
-        // Setează cursorul pe linia a doua, la început
+      // Set the cursor at the beginning of the second line
       lcd.setCursor(0, 1);
-      lcd.print("                ");
-      // Resetare poziție cursorului
+      lcd.print("                "); // Clear the line
+      // Reset cursor position
       lcd.setCursor(0, 1);
-      lcd.print(Nrmasini());
+      lcd.print(Nrmasini()); // Print the number of cars
     }
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
 
-
-
 bool turnOnLEDS() {
     if (xSemaphoreTake(sem_monitorizare, portMAX_DELAY) == pdTRUE){
-        // Verifică dacă intrarea este Blocata
+        // Check if the entry is blocked
         if (BlocareIesire || BlocareIntrare) {
             return false;
         }
-    if (xSemaphoreTake(sem_turnOnLED, portMAX_DELAY) == pdTRUE){
-    if (BlocareIesire || Panica) {
-        // Dacă intrarea este blocata,
-        // nu permite aprinderea LED-urilor
-        return false;
-    } else {
-        // Parcurge LED-urile și 
-        //aprinde primul LED care este în starea "off"
-        for (int i = 0; i < nr_max; i++) {
-            if (!StareLeduri[i]) {
-                digitalWrite(LED_MAXSINA[i], HIGH);
-                ledTimers[i] = millis();
-                StareLeduri[i] = true;
-                return true;
+        if (xSemaphoreTake(sem_turnOnLED, portMAX_DELAY) == pdTRUE){
+            if (BlocareIesire || Panica) {
+                // If the entry is blocked,
+                // do not allow the LEDs to be turned on
+                return false;
+            } else {
+                // Traverse the LEDs and
+                // turn on the first LED that is in the "off" state
+                for (int i = 0; i < nr_max; i++) {
+                    if (!StareLeduri[i]) {
+                        digitalWrite(LED_MAXSINA[i], HIGH);
+                        ledTimers[i] = millis();
+                        StareLeduri[i] = true;
+                        return true;
+                    }
+                }
+                return false;
             }
         }
-        return false;
-    }
-    }
     }
 }
 
@@ -204,32 +201,32 @@ void Iesire(void *pvParameters) {
   while(1) {
     if (xSemaphoreTake(sem_iesire, (TickType_t)10) == pdTRUE) {
        if (!BlocareIesire || Panica){
-      for (int i = 0; i < nr_max; i++) {
-        if (StareLeduri[i] && (millis() - ledTimers[i] >= interval)) {
-          digitalWrite(LED_MAXSINA[i], LOW);
-          StareLeduri[i] = false;
-          Serial.println("A iesit o masina din tunel.");
-          Serial.println(Nrmasini());
+            for (int i = 0; i < nr_max; i++) {
+                if (StareLeduri[i] && (millis() - ledTimers[i] >= interval)) {
+                    digitalWrite(LED_MAXSINA[i], LOW);
+                    StareLeduri[i] = false;
+                    Serial.println("A car has exited the tunnel.");
+                    Serial.println(Nrmasini());
 
-            if (Nrmasini() < nr_max) {
-              digitalWrite(LED_NR_MAX, LOW);
+                    if (Nrmasini() < nr_max) {
+                        digitalWrite(LED_NR_MAX, LOW);
+                    }
+                }
             }
         }
-        }
-       }
-       // Semnalizează iesirea
-      xSemaphoreGive(sem_iesire);
-      // Semnalizează turnOnLEDS
-      xSemaphoreGive(sem_turnOnLED);
-      // Semnalizează task-ul de monitorizare
-      xSemaphoreGive(sem_monitorizare);
-
+        // Signal the exit
+        xSemaphoreGive(sem_iesire);
+        // Signal turnOnLEDS
+        xSemaphoreGive(sem_turnOnLED);
+        // Signal the monitoring task
+        xSemaphoreGive(sem_monitorizare);
     }
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
 
-// Definiția task-ului buton de panică
+
+// Definition of the panic button task
 void Buton_Panica(void *pvParameters) {
   (void) pvParameters;
   while(1) {
@@ -237,22 +234,22 @@ void Buton_Panica(void *pvParameters) {
       vTaskDelay(50 / portTICK_PERIOD_MS);
       if (sem_intrareTake(sem_intrare, (TickType_t)10) == pdTRUE) {
         xSemaphoreTake(mutex_panica, portMAX_DELAY);
-        // Modifica modul de panică
+        // Toggle the panic mode
         Panica = !Panica;
         
         if (Panica) {
-          // Acctivarea modului de panică
+          // Activating panic mode
           BlocareIntrare = true;
           BlocareIesire = false;
           digitalWrite(LED_BUTON_PANICA, HIGH); 
-          // Aprinde LED-ul de panică
-          Serial.println("Butonul de panica a fost apasat");
+          // Light up the panic LED
+          Serial.println("Panic button pressed");
         } else {
-          // Dezactivarea modului de panică
+          // Deactivating panic mode
           BlocareIntrare = false;
           digitalWrite(LED_BUTON_PANICA, LOW); 
-          // Stinge LED-ul de panică
-          Serial.println("Intrare si iesire deBlocareIesiree");
+          // Turn off the panic LED
+          Serial.println("Entry and exit unblocked");
         }
         xSemaphoreGive(mutex_panica);
         sem_intrareGive(sem_intrare);
@@ -260,22 +257,23 @@ void Buton_Panica(void *pvParameters) {
     }
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
-}  
+}
 
+// Task for the gas sensor
 void Task_SenzorGaz(void *pvParameters) {
     while(1) {
-        int stareSenzor = digitalRead(PIN_SENSOR_GAZ);
+        int sensorStatus = digitalRead(PIN_SENSOR_GAZ);
 
-        if (stareSenzor == HIGH) {
-            // Detectare gaz
+        if (sensorStatus == HIGH) {
+            // Gas detected
             xSemaphoreTake(mutex_panica, portMAX_DELAY);
-            // Activarea modului de panică
+            // Activate panic mode
             Panica = true;
             xSemaphoreGive(mutex_panica);
-            Serial.println("Gaz detectat! Intrarea in tunel blocata.");
+            Serial.println("Gas detected! Tunnel entry blocked.");
         } else{
             xSemaphoreTake(mutex_panica, portMAX_DELAY);
-            // Dezactiveaza modul de panica
+            // Deactivate panic mode
             Panica = false;
             xSemaphoreGive(mutex_panica);
         }
@@ -286,21 +284,22 @@ void Task_SenzorGaz(void *pvParameters) {
     }
 }
 
+// Task for the smoke sensor
 void Task_SenzorFum(void *pvParameters) {
     while(1) {
-        int stareSenzor = digitalRead(PIN_SENSOR_FUM);
+        int sensorStatus = digitalRead(PIN_SENSOR_FUM);
 
-        if (stareSenzor == HIGH) {
-            // Detectare fum
+        if (sensorStatus == HIGH) {
+            // Smoke detected
             xSemaphoreTake(mutex_panica, portMAX_DELAY);
-            // Activarea modului de panică
+            // Activate panic mode
             Panica = true;
             xSemaphoreTake(mutex_panica);
-            Serial.println("Fum detectat! Intrarea in tunel blocata.");
+            Serial.println("Smoke detected! Tunnel entry blocked.");
         } else{
             xSemaphoreTake(mutex_panica, portMAX_DELAY);
-            // Dezactiveaza modul de panica
-            Panica = false; 
+            // Deactivate panic mode
+            Panica = false;
             xSemaphoreGive(mutex_panica);
         }
         xSemaphoreGive(sem_monitorizare);
@@ -310,14 +309,14 @@ void Task_SenzorFum(void *pvParameters) {
     }
 }
 
-
+// Function to count cars
 int Nrmasini() {
-    // Contor masini
+    // Car counter
     int numCars = 0;
-  for (int i = 0; i < nr_max; i++) {
-    if (StareLeduri[i]) {
-      numCars++;
+    for (int i = 0; i < nr_max; i++) {
+        if (StareLeduri[i]) {
+            numCars++;
+        }
     }
-  }
-  return numCars;
+    return numCars;
 }
